893cfb7df4f4c145383a4287b645047d16551710
nattytefera <nhattysafari@gamil.com>
2025-11-09 19:54:03 +0300

    first commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+const MainContent = () => {
+    return (
+        <main style={{ padding: '20px', backgroundColor: '#f9f9f9', borderRadius: '5px' }}>
+            <p style={{ fontSize: '18px', color: '#333' }}>
+                I love to visit <span style={{ fontWeight: 'bold', color: 'blue' }}>New York</span>, 
+                <span style={{ fontWeight: 'bold', color: 'blue' }}> Paris</span>, and 
+                <span style={{ fontWeight: 'bold', color: 'blue' }}> Tokyo</span>.
+            </p>
+        </main>
+    );
+};
+
+export default MainContent;
